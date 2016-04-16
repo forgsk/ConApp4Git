5da45cfef962a46420fbdc70dbb5196f8a997629
Suresh <forgsk@gmail.com>
2016-04-16 15:12:17 -0400

    up down line

--- ConApp4Git/Program.cs
+++ ConApp4Git/Program.cs
@@ -16,8 +16,8 @@ namespace ConApp4Git
             Console.WriteLine("Hello console4");
             Console.WriteLine("Hello console5");
             Console.WriteLine("Hello console6 modifiedin Git");
-            Console.WriteLine("Hello console7 in vs for merge wit git of console8");
             Console.WriteLine("Hello console8 in Git for merge wit vs of console7.");
+            Console.WriteLine("Hello console7 in vs for merge wit git of console8");
 
             Console.Read();
 
